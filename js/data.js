2a5824b924aebfb7feab1867943c433ad456cb29
Nikhil Hari <nikhil_hari@live.com>
2020-11-19 12:55:02 +0530

    Fix broken gallery

--- js/data.js
+++ js/data.js
@@ -1,14 +1,14 @@
 let data = {
     "Vinimay": [
-        "../assets/Vinimay/img3.jpg",
-        "../assets/Vinimay/img1.jpg",
-        "../assets/Vinimay/img2.jpg",
-        "../assets/Vinimay/img4.jpg"
+        "assets/Vinimay/img3.jpg",
+        "assets/Vinimay/img1.jpg",
+        "assets/Vinimay/img2.jpg",
+        "assets/Vinimay/img4.jpg"
     ],
     "Activities": [
-        "../assets/Gallery/img1.jpg",
-        "../assets/Gallery/img2.jpg",
-        "../assets/Gallery/img3.jpg",
-        "../assets/Gallery/img4.jpg"
+        "assets/Gallery/img1.jpg",
+        "assets/Gallery/img2.jpg",
+        "assets/Gallery/img3.jpg",
+        "assets/Gallery/img4.jpg"
     ]
 };
